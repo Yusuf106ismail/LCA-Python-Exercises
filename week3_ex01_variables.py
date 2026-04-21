aa9dbacae656a6b87ec023a2570108fc3585b0b7
Yusuf Ismail <myths07ismail@gmail.com>
2026-04-21 17:20:24 +0200

    week3_ex01_variables.py
    Completed Q1, Q2, Q3

--- week3_ex01_variables.py
+++ week3_ex01_variables.py
@@ -1,1 +1,25 @@
+# ask the user for their name
+name = input ("Enter your name")
+#ask the user for their age
+age = input (" Enter your age ")
+#print a greeting
+print(" Hello" + name + "you are" + age + " years old ")
+#ask for length and width ( convert to integers )
+length = int ( input( "Enter the length of the rectangle: "))
+width = int ( input("Enter the width of the rectangle: "))
+
+#calculate area
+area = length * width
+
+#print result
+print("The area of the rectangle is:",area)
+
+#ask for temperature in Celsius
+celsius = float ( input (" Enter temperature in celsius:"))
+
+#convert to fahrenheit
+Fahrenheit = ( celsius * 9/5) + 32
+
+#print to result rounded to 2 decimal places
+print (" Temperature in Fahrenheit: ", round (Fahrenheit ,2))
 
